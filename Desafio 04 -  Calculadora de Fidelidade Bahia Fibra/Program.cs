b4d60de3c33b9ba2a7917bcfaa1b99c3daa58b7d
Matheus <matheusmff06@gmail.com>
2026-03-26 08:41:17 -0300

    fix: organizando pastas dos desafios

---
+++ Desafio 04 -  Calculadora de Fidelidade Bahia Fibra/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Desafio_04____Calculadora_de_Fidelidade_Bahia_Fibra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========================================");
+            Console.WriteLine("===VALIDAÇÃO DE DESCONTO - BAHIA FIBRA===");
+            Console.WriteLine("===========================================");
+            Console.Write("Informe o seu nome completo: ");
+            String name = Console.ReadLine();
+            Console.Write("Informe a data da sua instalação: ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Console.Write("Informe o valor do seu plano: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            
+            TimeSpan dateDiference = DateTime.Now - date;
+            int diasTotais = dateDiference.Days;
+            double desconto;
+            
+            Console.WriteLine("--------------------------------------------");
+            if (diasTotais >= 365)
+            {
+                desconto = preco * 0.10;
+                double precoFinal = preco - desconto;
+                Console.WriteLine($"O cliente {name} está conosco há {diasTotais} dias.");
+                Console.WriteLine($"Ganhou 10% de desconto ({desconto:C2})!");
+                Console.WriteLine($"Valor final da mensalidade: {precoFinal:C2}");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"O cliente {name} está conosco há {diasTotais} dias.");
+                Console.WriteLine($"Ainda não possui desconto. Valor: {preco:C2}");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ Desafio 04 -  Calculadora de Fidelidade Bahia Fibra/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Desafio 04 -  Calculadora de Fidelidade Bahia Fibra")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Desafio 04 -  Calculadora de Fidelidade Bahia Fibra")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a83edd7a-965b-4eea-85bf-5ba4069fb97d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
