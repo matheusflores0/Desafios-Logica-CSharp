ee043ba76e09e874176c3c0f8e2186db721d776a
Matheus <matheusmff06@gmail.com>
2026-04-02 08:18:59 -0300

    feat: Adiciona Desafio 05 - Inventário de Roteadores

---
+++ Desafio 05 - Inventário de Roteadores/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_05___Inventário_de_Roteadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> routers = new List<String>();
+            Console.WriteLine("===== INVENTÁRIO DE EQUIPAMENTOS - BAHIA FIBRA =====");
+
+            for (int i = 1; i <= 5; i++) {
+                Console.WriteLine($"Digite o modelo do {i} roteador: ");
+                String modelo = Console.ReadLine();
+
+                routers.Add(modelo);
+
+            }
+            Console.Clear();
+            Console.WriteLine("======= RELATÓRIO FINAL DE ESTOQUE =======");
+
+            foreach (String item in routers)
+            {
+                Console.WriteLine($"- Modelo: {item}");
+            }
+        }
+    }
+}
---
+++ Desafio 05 - Inventário de Roteadores/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Desafio 05 - Inventário de Roteadores")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Desafio 05 - Inventário de Roteadores")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c2b42e1b-2956-4d64-934e-5eb67c0cc13f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
